d82773adcbdb9efdb9fb2b7ea668efa2767beb42
91juhwang <91juhwang@gmail.com>
2018-09-08 21:43:20 -0400

    Create User model and install Knock gem for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  include Knock::Authenticable
+
+  private
+
+  def unauthorized_entity(entity_name)
+    render json: { error: "Unauthorized Access" }, status: :unauthorized
+  end
 end
